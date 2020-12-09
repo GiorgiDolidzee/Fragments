455c2c5d8bf1778f33809a6764fd4cb72ec7ff81
GiorgiDolidzee <giorgi.dolidze.2@btu.edu.ge>
2020-12-09 22:05:53 +0400

    first commit

---
+++ app/src/main/java/com/example/fragments/allfragments/NotificationFragments.kt
@@ -0,0 +1,7 @@
+package com.example.fragments.allfragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class NotificationFragments: Fragment(R.layout.fragment_notification) {
+}
---
+++ app/src/main/java/com/example/fragments/allfragments/SettingFragment.kt
@@ -0,0 +1,7 @@
+package com.example.fragments.allfragments
+
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class SettingFragment: Fragment(R.layout.fragment_setting) {
+}
